7ad81955a61cbfbbacf69f042841cf09da38e312
javierg <javierg@amcoonline.net>
2012-07-29 13:48:30 -0700

    create proxy to get remote files

--- server.js
+++ server.js
@@ -1,4 +1,42 @@
 var connect = require('connect');
-connect.createServer(
-  connect.static(__dirname)
-).listen(8080);
+var http = require('http');
+var fs = require("fs");
+var url = require("url");
+
+var admin = function(req, res){
+  params = url.parse(req.url, true).query;
+  farm_host = 'farm' + params.h + '.staticflickr.com'
+  options = {
+    host: farm_host,
+    port: 80,
+    path: params.f
+  };
+  var request = http.get(options, function (result) {
+    var img = "";
+    result.setEncoding('binary');
+    result.on('data', function (chunk) {
+      if (result.statusCode == 200) {
+        img += chunk;
+      }
+    });
+    result.on('end', function () {
+      res.setHeader('Transfer-Encoding', 'chunked');
+      res.setHeader('Content-Type', 'image/jpeg');
+      res.end(img, 'binary');
+    });
+  });
+  request.on( 'error', function(e){
+    console.log(e);
+  });
+};
+
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.bodyParser())
+  .use( connect.static(__dirname))
+  .use('/img/tjmood', admin)
+  .use( function(req, res){
+    res.end('try /img/tjmood');
+  });
+
+http.createServer(app).listen(8080);
